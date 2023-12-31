07bd243c94a7aa5f64bac8c6c1a8e3c496eda813
jancarloonce <jancarloonce11@gmail.com>
2023-12-31 21:32:58 +0800

    Workaround for expired credly

--- src/components/home/Certifications.jsx
+++ src/components/home/Certifications.jsx
@@ -1,7 +1,8 @@
 // Certifications.jsx
-import React, { useState } from 'react';
-import { useSpring, animated, config } from 'react-spring';
-import { useInView } from 'react-intersection-observer';
+
+import React, { useState } from 'react';  // Import useState
+import { useSpring, animated, config } from 'react-spring';  // Import useSpring and animated
+import { useInView } from 'react-intersection-observer';  // Import useInView
 import './Certifications.css';
 
 const Certifications = ({ heading, certificationData }) => {
@@ -14,24 +15,33 @@ const Certifications = ({ heading, certificationData }) => {
 
   const springProps = useSpring({
     opacity: inView ? 1 : 0,
-    transform: inView ? 'translateX(0)' : 'translateX(50px)', // Adjust the distance and direction here
-    config: { ...config.slow, precision: 0.1 }, // Adjust the speed here (precision controls the smoothness)
+    transform: inView ? 'translateX(0)' : 'translateX(50px)',
+    config: { ...config.slow, precision: 0.1 },
   });
 
   const loadScript = (badgeId) => {
-    // Create script element
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = '//cdn.credly.com/assets/utilities/embed.js';
     script.async = true;
-
-    // Append script to the document body
     document.body.appendChild(script);
-
-    // Cleanup on component unmount
     return () => {
       document.body.removeChild(script);
     };
+  };
+
+  const coveringDivPosition = {
+    top: '170px',   // Adjust the top position
+    left: '80px',  // Adjust the left position
+  };
+  
+  const coveringDivStyle = {
+    position: 'absolute',
+    ...coveringDivPosition,
+    width: '30%',  // Adjust the width as needed
+    height: '10%', // Adjust the height as needed
+    backgroundColor: 'white',
+    zIndex: 1,
   };
 
   return (
@@ -44,7 +54,9 @@ const Certifications = ({ heading, certificationData }) => {
               key={index}
               className={`certification-badge ${selectedIndex === index ? 'selected' : ''}`}
               onClick={() => toggleSelection(index)}
+              style={{ position: 'relative' }}
             >
+              {index === 0 && <div style={coveringDivStyle}></div>}
               <div
                 data-iframe-width="150"
                 data-iframe-height="270"
